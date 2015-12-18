333e75267efc930333bda6f5dcfb17c7acdf4b0f
Apoorv Agrawal <apoorv.agrawal@outlook.com>
2015-12-18 17:53:41 +0530

    Fixed code styling

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-module.exports = (function(){
+module.exports = (function() {
 
-	var _lucene = function(_nano, _db, _config){
+  var _lucene = function(_nano, _db, _config) {
 
-		var db = _db;
-		var nano = _nano;
+    var db = _db;
+    var nano = _nano;
 
-		var config = _config || { fragment: '_fti' };
+    var config = _config || {
+      fragment: '_fti'
+    };
 
-		var _search = function(_design_doc, _view_name, _params, _callback) {
-		  var db_name = db.config.db;
+    var _search = function(_designDoc, _viewName, _params, _callback) {
+      var dbName = db.config.db;
 
-	      if (typeof _params === 'function') {
-	        _callback = _params;
-	        _params = {};
-	      }
+      if (typeof _params === 'function') {
+        _callback = _params;
+        _params = {};
+      }
 
-	      var view_path = config.fragment + '/local/' + db_name + '/_design/' + _design_doc + '/'  + _view_name;
-	      console.log(view_path);
+      var viewPath = config.fragment + '/local/' + dbName + '/_design/' + _designDoc + '/' +
+        _viewName;
 
-	      var req = {
-			uri: view_path,
-			method: 'GET',
-			path: view_path,
-			qs: _params
-  		  };
+      var req = {
+        uri: viewPath,
+        method: 'GET',
+        path: viewPath,
+        qs: _params
+      };
 
-	      return nano.relax(req, _callback);
+      return nano.relax(req, _callback);
+    };
 
-	    }
-
-		return {
-			search: _search
-		}
-
-
-	}
-
-	return _lucene;
+    return {
+      search: _search
+    };
+  };
+  return _lucene;
 })();
