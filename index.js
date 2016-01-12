c1f9e98cc7404b836f78c3887fed8846eeca7230
Apoorv Agrawal <apoorv.agrawal@outlook.com>
2016-01-12 18:07:51 +0530

    Rewrote the code according to the coding standards.

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-module.exports = (function() {
+var lucene = function(nanoAdapter, options) {
+  if (!nanoAdapter) {
+    throw new Error('Required Nano Adapter not Found');
+  }
+  var nano = nanoAdapter;
+  var database = options && options.db ? options.db : null;
+  var fragment = options && options.fragment ? options.fragment : '_fti';
 
-  var _lucene = function(_nano, _db, _config) {
+  var _search = function(designDoc, viewName, options, callback) {
 
-    var db = _db;
-    var nano = _nano;
+    if (!options) {
+      throw new Error('Required Options: Design Doc and View');
+    }
 
-    var config = _config || {
-      fragment: '_fti'
+    if (!designDoc) {
+      throw new Error('Required Options: Design Document');
+    }
+
+    if (!viewName) {
+      throw new Error('Required Options: View Name');
+    }
+
+    var queryString = options.queryString ? options.queryString : '';
+
+    var targetDatabase = options.db ? options.db : database;
+
+    if (!targetDatabase) {
+      throw new Error('Required: Database Name at Init or Search');
+    }
+
+    var viewPath = fragment + '/local/' + targetDatabase + '/_design/' +
+      designDoc + '/' + viewName;
+
+    var req = {
+      uri: viewPath,
+      method: 'GET',
+      path: viewPath,
+      qs: queryString
     };
 
-    var _search = function(_designDoc, _viewName, _params, _callback) {
-      var dbName = db.config.db;
-
-      if (typeof _params === 'function') {
-        _callback = _params;
-        _params = {};
-      }
-
-      var viewPath = config.fragment + '/local/' + dbName + '/_design/' + _designDoc + '/' +
-        _viewName;
-
-      var req = {
-        uri: viewPath,
-        method: 'GET',
-        path: viewPath,
-        qs: _params
-      };
-
-      return nano.relax(req, _callback);
-    };
-
-    return {
-      search: _search
-    };
+    return nano.relax(req, _callback);
   };
-  return _lucene;
-})();
+
+  return {
+    search: _search
+  };
+
+};
+
+module.exports = lucene;
